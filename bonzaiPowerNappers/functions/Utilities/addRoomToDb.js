6d463c36911123403a5d0c080a4e3044c8cdf4c1
kforsl <forslund95@live.com>
2024-09-09 11:28:50 +0200

    addRoomToDb function

---
+++ bonzaiPowerNappers/functions/Utilities/addRoomToDb.js
@@ -0,0 +1,49 @@
+const { sendError } = require("../../responses");
+
+async function addRoomToDb(roomType, floorNmbr, roomNmbr) {
+    let roomId = ""; 
+    let price = 0; 
+    let beds = 0; 
+    let isBooked = false; 
+
+    switch(roomType) {
+        case "single" : 
+            roomId = "SR"; 
+            price = 500; 
+            beds = 1; 
+            break;
+        case "double" : 
+            roomId = "DR"; 
+            price = 1000; 
+            beds = 2; 
+            break; 
+        case "suite" : 
+            roomId = "SU"; 
+            price = 1500; 
+            beds = 3; 
+            break; 
+        default: 
+            return { success: false, message: "No Rooms with that type"}
+    }
+
+    try {
+        await db.put({
+            TableName: "bonzai-rooms-db",
+            Item: {
+                roomType, 
+                roomId,
+                price, 
+                beds, 
+                floorNmbr, 
+                roomNmbr, 
+                isBooked
+                },
+       });
+
+       return { success: true }
+    } catch (error) {
+        return { success: false, message: error.message}
+    }
+}
+
+module.exports = { addRoomToDb }
