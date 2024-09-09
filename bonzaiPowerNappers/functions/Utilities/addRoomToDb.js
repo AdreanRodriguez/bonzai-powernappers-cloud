6f97be80e502449de1c542dda3d161fdd4a2c8ea
kforsl <forslund95@live.com>
2024-09-09 11:39:19 +0200

    tested ok

--- bonzaiPowerNappers/functions/Utilities/addRoomToDb.js
+++ bonzaiPowerNappers/functions/Utilities/addRoomToDb.js
@@ -1,48 +1,48 @@
-const { sendError } = require("../../responses");
+const { db } = require("../../services");
 
 async function addRoomToDb(roomType, floorNmbr, roomNmbr) {
-    let roomId = ""; 
-    let price = 0; 
-    let beds = 0; 
-    let isBooked = false; 
+    let roomId = "";
+    let price = 0;
+    let beds = 0;
+    let isBooked = false;
 
-    switch(roomType) {
-        case "single" : 
-            roomId = "SR"; 
-            price = 500; 
-            beds = 1; 
+    switch (roomType) {
+        case "single":
+            roomId = `SR${floorNmbr}${roomNmbr}`;
+            price = 500;
+            beds = 1;
             break;
-        case "double" : 
-            roomId = "DR"; 
-            price = 1000; 
-            beds = 2; 
-            break; 
-        case "suite" : 
-            roomId = "SU"; 
-            price = 1500; 
-            beds = 3; 
-            break; 
-        default: 
-            return { success: false, message: "No Rooms with that type"}
+        case "double":
+            roomId = `DR${floorNmbr}${roomNmbr}`;
+            price = 1000;
+            beds = 2;
+            break;
+        case "suite":
+            roomId = `SU${floorNmbr}${roomNmbr}`;
+            price = 1500;
+            beds = 3;
+            break;
+        default:
+            return { success: false, message: "No Rooms with that type" }
     }
 
     try {
         await db.put({
             TableName: "bonzai-rooms-db",
             Item: {
-                roomType, 
+                roomType,
                 roomId,
-                price, 
-                beds, 
-                floorNmbr, 
-                roomNmbr, 
+                price,
+                beds,
+                floorNmbr,
+                roomNmbr,
                 isBooked
-                },
-       });
+            },
+        });
 
-       return { success: true }
+        return { success: true }
     } catch (error) {
-        return { success: false, message: error.message}
+        return { success: false, message: error.message }
     }
 }
 
