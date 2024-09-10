738e25934b0603e6133a2320c302ab261bcaa9fc
Lennstorm <4824136@gmail.com>
2024-09-10 10:08:16 +0200

    deletebooking klar

--- bonzaiPowerNappers/functions/Utilities/deleteBookingInDb.js
+++ bonzaiPowerNappers/functions/Utilities/deleteBookingInDb.js
@@ -1,20 +1,20 @@
 const { sendError, sendResponse } = require("../../responses/index.js");
 const { db } = require("../../services/index.js");
 
-async function deleteBookingInDb(bookingId) {
+async function deleteBookingInDb( item ) {
     const params = {
         TableName: "bonzai-booking-db",
         Key: {
-            bookingId: bookingId
+            bookingId: item.bookingId
         }
     };
 
     try {
         await db.delete(params);
-        return sendResponse(200, `Booking ${bookingId} was successfully deleted.`);
+        return sendResponse(200, `Booking ${item.bookingId} was successfully deleted.`);
     } catch (error) {
         console.error("Error deleting booking", error.message);
-        return sendError(500, `Failed to delete booking ${bookingId}: ${error.message} !`);
+        return sendError(500, `Failed to delete booking ${item.bookingId}: ${error.message} !`);
     }
 }
 
