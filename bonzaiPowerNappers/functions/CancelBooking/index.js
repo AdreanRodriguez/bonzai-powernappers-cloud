6e760031721a31632472f3e14a2b532e1cf5d81d
AdreanRodriguez <loco_bladare@hotmail.com>
2024-09-10 10:25:18 +0200

    continue

--- bonzaiPowerNappers/functions/CancelBooking/index.js
+++ bonzaiPowerNappers/functions/CancelBooking/index.js
@@ -1,6 +1,14 @@
 const { sendResponse, sendError } = require("../../responses/index.js");
 const { db } = require("../../services/index.js");
+const { getOrder } = require("../Utilities/getOrder.js");
+const { deleteBookingInDb } = require("../Utilities/deleteBookingInDb.js");
 
 exports.handler = async (event) => {
-  return sendResponse(200, { message: "CancelBooking" });
+  const { id } = event.pathParameters;
+  const response = await getOrder(id);
+  if (!response.success) {
+    return sendError(404, response.message);
+  } else {
+    for (let i = 0; i < deleteBookingInDb.length; i++) {}
+  }
 };
