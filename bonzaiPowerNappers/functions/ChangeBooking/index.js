6e6fac6dfff4d6473e94595ebfb26fffed561dab
kforsl <forslund95@live.com>
2024-09-12 11:34:56 +0200

    first commit
    Co-authored-by: Adréan Rodriguez <AdreanRodriguez@users.noreply.github.com>
    Co-authored-by: ronnedahl <ronnedahl@users.noreply.github.com>
    Co-authored-by: Lennstorm <Lennstorm@users.noreply.github.com>

--- bonzaiPowerNappers/functions/ChangeBooking/index.js
+++ bonzaiPowerNappers/functions/ChangeBooking/index.js
@@ -1,9 +1,10 @@
 const { sendResponse, sendError } = require("../../responses/index.js");
 const { getOrder } = require("../Utilities/getOrder.js");
-const { deleteBookingInDb } = require("../Utilities/deleteBookingInDb.js");
-const { updateRoomStatus } = require("../Utilities/updateRoomStatus.js");
-const { getRoom } = require("../Utilities/getRoom.js");
-const { addBookingToDb } = require("../Utilities/addBookingToDb.js");
+const { compareNmbrOfPeople } = require("../Utilities/compareNmbrOfPeople.js");
+const { deleteAndUpdateStatus } = require("../Utilities/deleteAndUpdateStatus.js");
+const { addAndUpdateStatus } = require("../Utilities/addandUpdateStatus.js");
+const { compareChanges } = require("../Utilities/compareChanges.js");
+const { getRoomsToBook } = require("../Utilities/getRoomsToBook.js");
 
 exports.handler = async (event) => {
     try {
@@ -13,16 +14,31 @@ exports.handler = async (event) => {
         if (!nmbrOfGuests || !roomTypes || !checkIn || !checkOut) {
             return sendError(404, "Missing required fields: checkIn, checkOut, roomTypes or nmbrOfGuests.");
         }
+        // Kontroll att antal gäster stämmer med antal rumstyp
+        if (!compareNmbrOfPeople(nmbrOfGuests, roomTypes)) {
+            return sendError(
+                404,
+                "Wrong amount of people booked."
+            );
+        }
+        // hämtar alla ordrar som tillhör bookingId
         const orderResponse = await getOrder(id);
         if (!orderResponse.success) {
             return sendError(404, orderResponse.message);
         }
 
-        let totalPrice = 0;
-        let nmbrOfBookedGuests = 0;
-        const bookedRooms = [];
-        const nmbrOfBookedNights = checkOut.getDay() - checkIn.getDay();
+        const checkInDate = new Date(checkIn);
+        const checkOutDate = new Date(checkOut);
+        const numberOfNights = Math.ceil(
+            (checkOutDate.getTime() - checkInDate.getTime()) / (1000 * 60 * 60 * 24)
+        );
+        if (numberOfNights < 1) {
+            return sendError(404, "Please check dates!");
+        }
         const bookingInformation = {
+            checkIn: checkInDate.toLocaleDateString("se-SV"),
+            checkOut: checkOutDate.toLocaleDateString("se-SV"),
+            bookingId: id,
             roomTypes,
             nmbrOfGuests,
             checkIn: new Date(checkIn),
@@ -31,54 +47,39 @@ exports.handler = async (event) => {
             guestEmail: orderResponse.items[0].guestEmail,
             guestName: orderResponse.items[0].guestName,
         };
+        // compareChanges är en function som kontrollerar hur tidigare ordrar ska hanteras 
+        // oldOrders är ordrar som inte matchar roomTypes och ska tas bort
+        // roomsToSave är ordrar som matchar roomTypes och ska sparas
+        // roomTypesToAdd är de extra typerna som måste bokas  
+        const { oldOrders, roomsToSave, roomTypesToAdd, savedTotalPrice } = compareChanges(orderResponse.items, roomTypes);
 
-        for (let i = 0; i < orderResponse.items.length; i++) {
-            const deleteResponse = await deleteBookingInDb(orderResponse.items[i]);
-            if (!deleteResponse.success) {
-                return sendError(404, deleteResponse.message);
-            }
-
-            const updateResponse = await updateRoomStatus(orderResponse.items[i], false);
-            if (!updateResponse.success) {
-                return sendError(404, updateResponse.message);
-            }
+        // tar bort bokningar från databasen och updaterar isBooked för rum
+        const deleteResponse = await deleteAndUpdateStatus(oldOrders);
+        if (!deleteResponse.success) {
+            return sendError(404, deleteResponse.message);
         }
 
-        for (let i = 0; i < roomTypes.length; i++) {
-            const roomResponse = await getRoom(roomTypes[i]);
-            if (!roomResponse.success) {
-                return sendError(404, updateResponse.message);
-            }
+        // getRoomsToBook är en funktion som hämtar alla de rum som användaren vill boka och lägger dem i en array
+        const { success, bookedRooms, totalPrice, message } = await getRoomsToBook(roomTypesToAdd);
+        if (!success) {
+            return sendError(404, message);
+        }
 
-            const room = roomResponse.item;
-            const updateResponse = await updateRoomStatus(room, true);
-            if (!updateResponse.success) {
-                return sendError(404, updateRespons.message);
-            }
+        // lägger till bokade rum i databasen och updaterar isBooked-status på rum.
+        const response = await addAndUpdateStatus(bookedRooms, bookingInformation);
+        if (!response.success) {
+            return sendError(404, response.message);
+        }
+
+        // Lägger till de rum som redan fanns i bokningsdatabasen och som tillhör ordern.
+        roomsToSave.forEach(room => {
             bookedRooms.push(room);
-            totalPrice += room.price;
-            nmbrOfBookedGuests += room.beds;
-        }
+        });
 
-        if (nmbrOfBookedGuests !== nmbrOfGuests) {
-            for (let i = 0; i < bookedRooms.length; i++) {
-                const updateResponse = await updateRoomStatus(room, true);
-                if (!updateResponse.success) {
-                    return sendError(404, updateRespons.message);
-                }
-            }
-            return sendError(404, "Not right amount of people booked");
-        }
         bookingInformation.bookedRooms = bookedRooms;
-        bookingInformation.totalPrice = totalPrice *= nmbrOfBookedNights;
+        bookingInformation.totalPrice = (totalPrice + savedTotalPrice) * numberOfNights;
 
-        for (let i = 0; i < bookedRooms.length; i++) {
-            const bookingResponse = await addBookingToDb(bookingInformation, bookedRooms[i]);
-            if (!bookingResponse.success) {
-                return sendError(404, bookingResponse.message);
-            }
-        }
-        return sendResponse(200, { message: "ChangeBooking" });
+        return sendResponse(200, bookingInformation);
     } catch (error) {
         return sendError(404, error.message);
     }
